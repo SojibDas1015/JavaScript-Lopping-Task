4f3ed31e2873b7f9927db75d2f8346a16965f742
Sojib Das <sojibdas1015@gmail.com>
2025-12-28 11:41:48 +0600

    task 6: Given an array of student objects, print each student’s name and marks

---
+++ 6Task.js
@@ -0,0 +1,12 @@
+let ar = [
+    { name: "John", marks: 85 },
+    { name: "Alice", marks: 90 }
+]
+for(let ob of ar)
+{
+    console.log(ob.name, 'scored' , ob.marks)
+    // for(let word in ob)
+    // {
+    //     console.log(ob[word]);
+    // }
+}
